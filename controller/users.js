44eaec8dca48724824ab9ac20aa7923b5cec49bd
nOSkr <122243978+nOSkr04@users.noreply.github.com>
2024-02-13 09:31:11 +0800

    sync

--- controller/users.js
+++ controller/users.js
@@ -6,6 +6,7 @@ import sendNotification from "../utils/sendNotification.js";
 
 export const authMeUser = asyncHandler(async (req, res) => {
   const user = await User.findById(req.userId);
+  console.log(user);
   if (!user) {
     throw new MyError(req.params.id, 401);
   }
